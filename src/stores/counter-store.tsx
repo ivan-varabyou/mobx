9a95ca5e02cd5fecc9227f2fa796ae04b0816078
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-22 12:48:32 +0300

    feat: add mobx store counter

---
+++ src/stores/counter-store.tsx
@@ -0,0 +1,30 @@
+import { makeAutoObservable } from "mobx";
+
+class CounterStore {
+  count = 0;
+
+  constructor() {
+    // makeObservable(this, {
+    //   count: observable,
+    //   increment: action,
+    //   decrement: action,
+    //   total: computed,
+    // });  OR MakeAutoObservable
+
+    makeAutoObservable(this); // auto create makeObservable
+  }
+
+  get total() {
+    return this.count * 2;
+  }
+
+  increment = (value: number) => {
+    this.count += value;
+  };
+
+  decrement = (value: number) => {
+    this.count -= value;
+  };
+}
+
+export default new CounterStore();
